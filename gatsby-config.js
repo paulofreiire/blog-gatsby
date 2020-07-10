e03264d05a001a8a16dbc9ebc976c02dc95b0fdc
Paulo Freire <paulo.freiire@hotmail.com>
2020-07-10 15:01:48 -0300

    clean starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Casa do Estudante do Ceará`,
+    description: `Site sobre a Moradia Estudantil Casa do Estudante do Ceará`,
+    author: `@paulofreiredev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
